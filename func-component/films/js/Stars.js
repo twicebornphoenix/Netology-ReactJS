71f0d092ef5c2b70b76fc008fd3273e86bef0f28
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-09-14 13:46:28 +0300

    done

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars({ count }) {
+	let isVoted = false;
+	let stars = [];
+	
+	if (count <= 5 && count >= 1 && typeof count === 'number') {
+		isVoted = true;
+		stars = Array.prototype.constructor(count).fill('.').map(star => <Star />);
+	}
+
+  return (
+  	<ul className="card-body-stars u-clearfix">
+  		<li>
+  			{ isVoted && stars}
+  		</li>
+	  </ul>
+  )
 }
