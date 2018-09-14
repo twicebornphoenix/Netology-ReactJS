3cdde466b75d1b59d5e429587ffe76a5067032e7
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-09-14 14:24:49 +0300

    done

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function MessageHistory({ list }) {
+	if (!list.length) return;
+
+	const messageArr = list.map(message => {
+
+		switch( message.type ) {
+			case 'message': 
+				return <Message from={message.from} message={message} />
+			case 'response':
+				return <Response from={message.from} message={message} />
+			case 'typing':
+				return <Typing from={message.from} message={message} />
+		}
+
+	})
+
+	return (
+			<ul>
+				{ messageArr }
+			</ul>
+		)
+}
