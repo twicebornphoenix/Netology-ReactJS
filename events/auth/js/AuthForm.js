ce57f192cf05edb8e2be6fe81a20ddec8a5391cb
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-09-20 23:42:01 +0300

    ...

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function AuthForm(props) {
+	return (
+		<form action="/404/auth/" className="ModalForm" method="POST">
+			<div className="Input">
+				<input type="text" required placeholder="Имя"/>
+				<label></label>
+			</div>
+			<div className="Input">
+				<input type="email" placeholder="Электронная почта"/>
+				<label></label>
+			</div>
+			<div className="Input">
+				<input type="password" required placeholder="Пароль"/>
+				<label></label>
+			</div>
+			<button type="submit">
+				<span>Войти</span>
+				<i className="fa fa-fw fa-chevron-right"></i>
+			</button>
+		</form>
+
+		)
+}
