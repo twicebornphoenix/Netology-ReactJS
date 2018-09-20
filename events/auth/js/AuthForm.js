204286302318dcaa686d7ea5df99f62d12da54a8
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-09-21 00:04:55 +0300

    ...

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function AuthForm(props) {
+
+	function getData(e) {
+		e.preventDefault();
+		if (typeof props.onAuth === 'function') {
+			const data = {
+				'name': document.querySelector('[type=text]').value,
+				'email': document.querySelector('[type=email').value,
+				'password': document.querySelector('[type=password').value
+			}
+			props.onAuth(data);
+		}
+	}
+
 	return (
-		<form action="/404/auth/" className="ModalForm" method="POST">
+		<form action="/404/auth/" className="ModalForm" method="POST" onSubmit={getData}> 
 			<div className="Input">
 				<input type="text" required placeholder="Имя"/>
 				<label></label>
@@ -20,6 +33,5 @@ function AuthForm(props) {
 				<i className="fa fa-fw fa-chevron-right"></i>
 			</button>
 		</form>
-
 		)
 }
