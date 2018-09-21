f0ad7efc7b818e585c6e5013929614df878dfa09
twiceborn.phoenix <twiceborn.phoenix@gmail.com>
2018-09-21 15:46:52 +0300

    done

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,24 +1,19 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
+'use strict';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       active: this.props.options[0],
       open: false
-    };
-  },
-  handleChange: function (option) {
-    this.setState({
-      active: option
-    });
-  },
-  toggleOpen: function () {
-    this.setState({
-      open: !this.state.open
-    });
-  },
-  render: function () {
+    }
+  }
+  
+  handleChange = (option) => this.setState({active: option})
+
+  toggleOpen = () => this.setState({open: !this.state.open})
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
@@ -37,6 +32,49 @@ var App = React.createClass({
           </ul>
         </div>
       </div>
-    );
+      )
   }
-});
+}
+
+// var App = React.createClass({
+//   defaultProps: {
+//     options: []
+//   },
+//   getInitialState: function () {
+//     return {
+//       active: this.props.options[0],
+//       open: false
+//     };
+//   },
+//   handleChange: function (option) {
+//     this.setState({
+//       active: option
+//     });
+//   },
+//   toggleOpen: function () {
+//     this.setState({
+//       open: !this.state.open
+//     });
+//   },
+//   render: function () {
+//     return (
+//       <div className="container">
+//         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
+//           <button className={"btn"} onClick={this.toggleOpen} >
+//             <span>Account Settings</span>
+//             <i className="material-icons">public</i>
+//           </button>
+//           <ul className="dropdown">
+//             {this.props.options.map((option, i) => (
+//               <li
+//                 className={option === this.state.active ? "active" : ""}
+//                 onClick={() => this.handleChange(option)} >
+//                   <a href="#">{option}</a>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// });
