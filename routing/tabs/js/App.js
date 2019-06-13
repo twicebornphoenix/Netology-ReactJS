e05cfba98c1227028904de9235c688dd5ace8062
Twice Born Phoenix <twiceborn.phoenix@gmail.com>
2019-06-13 13:25:03 +0300

    component App

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,18 @@
-
+const App = () => (
+	<Router>
+		<div className='tabs'>
+			<nav className='tabs__items'>
+				<NavLink exact className='tabs__item' to='/' activeClassName='tabs__item-active'>Рефераты</NavLink>
+				<NavLink exact className='tabs__item' to='/creator' activeClassName='tabs__item-active'>Криэйтор</NavLink>
+				<NavLink exact className='tabs__item' to='/fortune' activeClassName='tabs__item-active'>Гадалка</NavLink>
+			</nav>
+			<div className='tabs__content'>
+				<Switch>
+					<Route path='/fortune' component={Fortune} />
+					<Route path='/creator' component={Creator} />
+					<Route path='/' component={Essay} />
+				</Switch>
+			</div>
+		</div>
+	</Router>
+);
